a56a9cc15604de14ef73db97ab0dbe1171701009
youssefibrahim22 <youssefibrahim0711@gmail.com>
2024-08-01 12:24:16 +0300

    add changes

--- index.js
+++ index.js
@@ -24,7 +24,20 @@ const model = {
   ],
   addTodo: function (todo) {
     this.todos.push(todo);
-    view.renderTodo(todo.title);
+    view.renderAllTodos();
+  },
+  removeTodo: function (id) {
+    const index = this.todos.findIndex((t) => t.id === id);
+    if (index > -1) {
+      this.todos.splice(index, 1);
+      view.renderAllTodos();
+    }
+  },
+  markAsCompleted: function (id) {
+    const todo = this.todos.find((t) => t.id === id);
+    if (todo) {
+      todo.completed = true;
+    }
   },
   getTodos: function () {
     return this.todos;
@@ -33,41 +46,60 @@ const model = {
 
 const view = {
   init: function () {
-    const todos = model.getTodos();
-    todos.forEach((todo) => this.renderTodo(todo.title));
+    this.renderAllTodos();
   },
-  renderTodo: function (title) {
-    const todoElem = `<li><p>${title}</p>
-                           <button class="circle check">
-                           <i class="fas fa-check-circle"></i>
-                           </button>
-
-                           <button class="circle delete">
-                           <i class="fas fa-trash"></i>
-                           </button></li>`;
-    const todoListElem = document.getElementById("todosList");
-
+  renderTodo: function (todo) {
+    const completedClass = todo.completed ? "completed" : "";
+    const todoElem = `<li class="text-Slot ${completedClass}" data-id="${todo.id}">
+      <div class="small-txt-Field">${todo.title}</div>
+      <button class="done-Btn" type="button">
+        <i class="fas fa-check"></i>
+      </button>
+      <button class="delete-Btn" type="button">
+        <i class="fas fa-trash-alt"></i>
+      </button></li>`;
+    const todoListElem = document.getElementById("tasks-List");
     todoListElem.innerHTML += todoElem;
-    return;
+  },
+  renderAllTodos: function () {
+    const todoListElem = document.getElementById("tasks-List");
+    todoListElem.innerHTML = "";
+    model.getTodos().forEach((todo) => this.renderTodo(todo));
   },
 };
 
 const controller = {
   init: function () {
     this.handleAddTodo();
+    this.handleTodoActions();
   },
   handleAddTodo: function () {
-    const formElem = document.getElementById("myForm");
+    const formElem = document.getElementById("task-Form");
     formElem.addEventListener("submit", function (e) {
       e.preventDefault();
-      const inputElemVal = document.getElementById("in").value;
+      const inputElemVal = document.getElementById("task-Input").value;
+      const id = (model.getTodos()[model.getTodos().length - 1]?.id ?? 0) + 1;
+
       model.addTodo({
-        id: model.getTodos()[model.getTodos().length - 1].id + 1,
+        id,
         title: inputElemVal,
         completed: false,
         created_at: "",
         updated_at: "",
       });
+    });
+  },
+  handleTodoActions: function () {
+    const todoListElem = document.getElementById("tasks-List");
+    todoListElem.addEventListener("click", function (e) {
+      if (e.target.closest(".delete-Btn")) {
+        const todoId = parseInt(e.target.closest("li").dataset.id);
+        model.removeTodo(todoId);
+      } else if (e.target.closest(".done-Btn")) {
+        const todoId = parseInt(e.target.closest("li").dataset.id);
+        model.markAsCompleted(todoId);
+        view.renderAllTodos();
+      }
     });
   },
 };
