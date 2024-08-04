4d263de92b3f4c6354996a63fbdf316918429401
Ahmed Elgalaly <Ahmedelgalaly.441@yahoo.com>
2024-08-04 12:28:39 +0300

    added all CRUD operations

--- main.js
+++ main.js
@@ -1,5 +1,9 @@
 
 const model = {
+    init: async function () {
+        this.todos = await this.fetchTodos();
+    },
+
     todos: [
         {
             id: 1,
@@ -8,15 +12,9 @@ const model = {
             created_at: "",
             updated_at: "",
         },
-        {
-            id: 2,
-            title: "task 2",
-            completed: false,
-            created_at: "",
-            updated_at: "",
-        },
 
     ],
+
 
     addTodo: function (todo){
         this.todos.push(todo);
@@ -29,9 +27,96 @@ const model = {
 
     getTodos: function (){
         return this.todos;
-    }
+    },
+
+    fetchTodos: async function () {
+        try {
+            const res = await fetch("http://127.0.0.1:8000/api/todos", {
+                method: "GET",
+            });
+            if (!res.ok) {
+                throw new Error(`HTTP error! status: ${res.status}`);
+            }
+            const data = await res.json();
+            return data.todos;
+        } catch (error) {
+            console.error('Error fetching todos:', error);
+        }
+    },
+
+    saveTodo: async function (title,completed) {
+        try {
+            const res = await fetch("http://127.0.0.1:8000/api/todos", {
+                method: "POST",
+                headers: {
+                    'Accept': 'application/json',
+                    'Content-Type': 'application/json'
+                },
+                body: JSON.stringify({
+                    "title": title,
+                    "completed": completed
+                })
+            });
+            if (!res.ok) {
+                throw new Error(`HTTP error! status: ${res.status}`);
+            }
+            const data = await res.json();
+            return data.todo;
+        } catch (error) {
+            console.error('Error saving todo:', error);
+        }
+    },
+
+    deleteTodoFromDB : async function (todoId){
+        try {
+            const url = `http://127.0.0.1:8000/api/todos/${todoId}`;
+            
+            const res = await fetch(url, {
+                method: "DELETE",
+
+                headers: {
+                    'Accept': 'application/json',
+                    'Content-Type': 'application/json',
+                    "Access-Control-Allow-Methods" :"GET, PUT, POST, UPDATE, DELETE, OPTIONS"
+            }});
+            if (!res.ok) {
+                throw new Error(`HTTP error! status: ${res.status}`);
+            }
+            const data = await res.json();
+            return data;
+
+        } catch (error) {
+            console.error('Error deleting todo:', error);
+        }
+    },
+
+    updateTodo: async function (todo){
+        console.log(todo);
+        try {
+            const url = `http://127.0.0.1:8000/api/todos/${todo.id}`;
+            const res = await fetch(url, {
+                method: "PUT",
+                headers: {
+                    'Accept': 'application/json',
+                    'Content-Type': 'application/json'
+                },
+                body: JSON.stringify({
+                    "title": todo.title,
+                    "completed": todo.completed
+                })
+            });
+            if (!res.ok) {
+                throw new Error(`HTTP error! status: ${res.status}`);
+            }
+            const data = await res.json();
+            return data.todo;
+        } catch (error) {
+            console.error('Error updating todo:', error);
+        }
+    },
 
 };
+
 
 const view = {
     init: function (){
@@ -56,7 +141,7 @@ const view = {
         input.readOnly = true;
 
         input.addEventListener('click', () => {
-            console.log("clicked");
+            
             input.readOnly = false;
             input.focus();
         });
@@ -65,6 +150,8 @@ const view = {
         // Action when out of focus
         input.addEventListener('blur', () => {
             input.readOnly = true;
+            todo.title = input.value;
+            controller.handleUpdateTodo(todo);
         })
 
         return input;
@@ -95,7 +182,7 @@ const view = {
         // Adding event listeners to buttons 
         deleteBtn.addEventListener('click', () =>{
             listElement.remove();
-            controller.handleDelteTodo(todo.id);
+            controller.handleDeleteTodo(todo.id);
         });
 
         doneBtn.addEventListener('click', () =>{
@@ -104,6 +191,7 @@ const view = {
                 input.style.backgroundColor = 'green';
                 input.style.color = 'white';
                 todo.completed = true;
+                controller.handleUpdateTodo(todo);
 
             }
             else{
@@ -111,6 +199,7 @@ const view = {
                 input.style.backgroundColor = '#FFFFFF';
                 input.style.color = 'black';
                 todo.completed = false;
+                controller.handleUpdateTodo(todo);
             }
         });
     
@@ -142,7 +231,7 @@ const view = {
         const parent = document.getElementById('taskList');
         parent.insertBefore(listElement,parent.firstChild);
 
-        console.log(model.getTodos());
+        
     }
 }
 
@@ -153,31 +242,44 @@ const controller = {
     handleAddTodo: function (){
         const formElement = document.getElementById("taskForm");
 
-        formElement.addEventListener("submit",(event => {
+        formElement.addEventListener("submit", (async event => {
 
             event.preventDefault();
         
             const formData = new FormData(event.target);
             const taskTitle = formData.get("textInput");
 
-            model.addTodo({
-                id: model.getTodos()[model.getTodos().length -1].id +1,
-                title: taskTitle,
-                completed: false,
-                created_at: Date().toString(),
-                updated_at: Date().toString(),
-            });
-        
-            document.getElementById("mainInput").value = "";
-        }));
+            const dbTodo = await model.saveTodo(taskTitle, false);
+            
+
+            const newTodo = {
+                    id: dbTodo.id,
+                    title: dbTodo.title,
+                    completed: dbTodo.completed,
+                    created_at: dbTodo.created_at,
+                    updated_at: dbTodo.updated_at,
+                };
+                model.addTodo(newTodo);
+                
+
+                document.getElementById("mainInput").value = "";
+        }
+    
+        ));},
+
+    handleDeleteTodo: function (todoId){
+        model.deleteTodo(todoId);
+        model.deleteTodoFromDB(todoId);
     },
 
-    handleDelteTodo: function (todoId){
-        model.deleteTodo(todoId)
+    handleUpdateTodo: function (todo){
+        model.updateTodo(todo);
+
     }
 }
 
-document.addEventListener("DOMContentLoaded", ()=> {
+document.addEventListener("DOMContentLoaded", async ()=> {
+    await model.init();
     controller.init();
     view.init()
 })
